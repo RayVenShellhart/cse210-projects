a5a64e120acff3834b93c8097519fe8b3f85927c
RayVenShellhart <rayvenshellhart@gmail.com>
2025-04-01 11:29:57 -0700

    inherit

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Sam", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Robert", "Fractions", "7.3", "8-9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssingment a3 = new WritingAssingment("mary", "history", "World war II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInfo());
     }
 }
